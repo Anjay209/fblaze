1501f19a071876ae645a30033903e112351bf814
Anjay209 <anjaypartha09@gmail.com>
2026-02-10 19:30:27 -0800

    Load shared Firebase initializer on all pages

---
+++ docs/js/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYp9sBwOWBdu9W46Q6XFp9zfLCrEsaO4",
+  authDomain: "certquest-94959.firebaseapp.com",
+  projectId: "certquest-94959",
+  storageBucket: "certquest-94959.firebasestorage.app",
+  messagingSenderId: "323956529033",
+  appId: "1:323956529033:web:e9c9c6a3c7668b8a72f358",
+  measurementId: "G-F5JHTGGN6K"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
